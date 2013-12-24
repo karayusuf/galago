8c3fd2f57f313e49e5cec6dc2bbd59e8599d316e
Joe Karayusuf <jkarayusuf@gmail.com>
2013-12-24 12:47:48 -0800

    Move Application into galago.rb

--- lib/galago.rb
+++ lib/galago.rb
@@ -6,7 +6,18 @@ require "galago/router/endpoint"
 require "galago/router/errors"
 require "galago/router/path"
 
-require "galago/application"
-
 module Galago
+  class Application
+    def self.router
+      @router ||= Router.new
+    end
+
+    def self.routes(&block)
+      Router::DSL.new(router, block)
+    end
+
+    def self.call(env)
+      router.process_request(env)
+    end
+  end
 end
