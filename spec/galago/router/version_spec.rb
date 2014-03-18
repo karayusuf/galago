868e97e3918f639a00d0b0b58f7d9e284a4b4cf6
Joe Karayusuf <jkarayusuf@gmail.com>
2014-03-17 22:57:19 -0700

    Bump 0.1.1

--- spec/galago/router/version_spec.rb
+++ spec/galago/router/version_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Galago
   describe 'Router::VERSION' do
     it "current" do
-      expect(Router::VERSION).to eql '0.1.0'
+      expect(Router::VERSION).to eql '0.1.1'
     end
   end
 end
